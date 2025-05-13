acab5160e053b6aeadbf6456868688692cd2cbea
MA Ahi <bijoyrashidul21@gmail.com>
2025-05-13 10:21:13 +0600

    new update

--- models/TurntableMenu.js
+++ models/TurntableMenu.js
@@ -6,17 +6,10 @@ const turntableMenuSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   description: { type: String, required: true },
   rating: { type: Number, default: 0 },
+  reviews: { type: Number, default: 0 },
   soldOut: { type: Boolean, default: false },
   stock: { type: Number, required: true },
   warranty: { type: String, default: "1 year limited warranty" },
-  reviews: [
-    {
-      userId: String,
-      comment: String,
-      rating: Number,
-      createdAt: { type: Date, default: Date.now },
-    },
-  ],
 });
 
 module.exports = mongoose.model("TurntableMenu", turntableMenuSchema);
